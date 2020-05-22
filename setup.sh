c27806e7e3365bd8ddaa9008c73056e6177e9bb1
fahimghl <fahim.ghl@hotmail.co.uk>
2020-05-22 05:34:23 +0100

    add setup file

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VERSION='1.0.0'
+TITLE='macOS Provisioner'
+
+echo"Instslling Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew tap Homebrew/bundle
